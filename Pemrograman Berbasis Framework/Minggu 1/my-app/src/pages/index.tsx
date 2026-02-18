65a54a482fc7573cd4b67e3c77709d669a4cec48
Luthfi27Upi <galut012@github.com>
2026-02-18 15:09:16 +0700

    JS 1 / Tugas 2

--- Pemrograman Berbasis Framework/Minggu 1/my-app/src/pages/index.tsx
+++ Pemrograman Berbasis Framework/Minggu 1/my-app/src/pages/index.tsx
@@ -1,15 +1,15 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
-
-const inter = Inter({ subsets: ['latin'] })
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>
-      <h1> Praktikum Next.js page router </h1>
-      <p> Mahasiswa D4 Pengembangan Web </p>
-    </div>
-  )
-}    
+    <main style={{ padding: "20px" }}>
+      <h1>Praktikum Next.js Pages Router</h1>
+
+      <Link href="/about">
+        <button style={{ padding: "10px 15px", cursor: "pointer" }}>
+          Tentang Saya
+        </button>
+      </Link>
+    </main>
+  );
+}
