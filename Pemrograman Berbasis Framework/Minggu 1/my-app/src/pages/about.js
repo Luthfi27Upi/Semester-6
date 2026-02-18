21276005c781922e81e6575fdc2bfda2530da604
Luthfi27Upi <galut012@github.com>
2026-02-18 14:55:51 +0700

    JS / Tugas 1

---
+++ Pemrograman Berbasis Framework/Minggu 1/my-app/src/pages/about.js
@@ -0,0 +1,13 @@
+export default function About() {
+  return (
+    <main style={{ padding: "20px" }}>
+      <h1>About Me</h1>
+
+      <ul>
+        <li><strong>Nama:</strong> Luthfi Triaswangga</li>
+        <li><strong>NIM:</strong> 2341720208</li>
+        <li><strong>Program Studi:</strong> D4 Teknik Informatika / wTI 3D / 18</li>
+      </ul>
+    </main>
+  );
+}
