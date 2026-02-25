f8e5072c2a61e0c2ef2215bac89f9a4dad77e555
Luthfi27Upi <galut012@github.com>
2026-02-26 00:13:30 +0700

    Tugas 3 - Layout

--- Pemrograman Berbasis Framework/Minggu 2/my-app/src/components/layouts/Appshell/index.tsx
+++ Pemrograman Berbasis Framework/Minggu 2/my-app/src/components/layouts/Appshell/index.tsx
@@ -1,18 +1,19 @@
-import Navbar from "../navbar";
+import Navbar from "@/components/layouts/navbar";
+import Footer from "@/components/layouts/footer";
 
-type AppShellProps = {
-    children: React.ReactNode;
-}
-
-const AppShell = (props: AppShellProps) => {
-    const { children } = props;
-
-    return (
-        <main>
-            <Navbar />
-            {children}
-        </main>
-    );
+type AppshellProps = {
+  children: React.ReactNode;
 };
 
-export default AppShell;
+const Appshell = (props: AppshellProps) => {
+  const { children } = props;
+  return (
+    <main>
+      <Navbar />
+      {children}
+      <Footer />
+    </main>
+  );
+};
+
+export default Appshell;
